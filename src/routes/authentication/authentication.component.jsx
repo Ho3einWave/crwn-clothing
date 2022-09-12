91fb804feea6c4af453367c10614ab0b0ee622aa
Hosein <48409432+Ho3einWave@users.noreply.github.com>
2022-09-12 17:52:18 +0430

    auth fix

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,7 +1,7 @@
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 
-import "./authetication.styles.scss";
+import "./authentication.styles.scss";
 
 const Authentication = () => {
   return (
